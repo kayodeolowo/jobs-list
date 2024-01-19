2a1b9ef7d080c73c249e1aa16f3a127954529fd8
kayodeolowo <officialkayodeolowo@gmail.com>
2024-01-19 10:50:35 +0100

    initial set up, prod, dev

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ('express')
+const app = express();
+const dotenv= require('dotenv')
+
+//setting up config.env file variables
+dotenv.config({path: './config/config.env'})
+
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, ()=>{
+    console.log(`server starter on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+
+});
