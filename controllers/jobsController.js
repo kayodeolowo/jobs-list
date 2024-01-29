4f1370f36ce00ead5e220f93ca8d8c6076dd7337
kayodeolowo <officialkayodeolowo@gmail.com>
2024-01-29 15:18:33 +0100

    all jobs

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -2,23 +2,26 @@ const Job = require('../models/jobs')
 
 //Get all jobs (/api/v1/jobs)
 
-exports.getJobs = (req, res, next )=>{
+exports.getJobs = async (req, res, next) => {
+
+    const jobs = await Job.find()
+
     res.status(200).json({
-        success : true,
-        requestMethod: req.requestMethod,
-        message: 'this route will display obs fr new data base'
+        success: true,
+        results: jobs.length,
+        data : jobs
     })
 }
 
 //create a new job => /api/v1/job/new
 
-exports.newJob = async (req, res, next)=>{
+exports.newJob = async (req, res, next) => {
     //console.log(req.body)
-   const job = await Job.create(req.body);
-   
-   res.status(200).json({
-    success : true,
-    message: 'job created',
-    data: job
-   })
+    const job = await Job.create(req.body);
+
+    res.status(200).json({
+        success: true,
+        message: 'job created',
+        data: job
+    })
 }
