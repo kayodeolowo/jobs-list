9c7d21c91812d056c3592d6c69ead3cb44db3918
kayodeolowo <officialkayodeolowo@gmail.com>
2024-02-14 12:09:21 +0100

    jwt schema

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
 //create user
 const userSchema = new mongoose.Schema({
@@ -49,12 +49,12 @@ userSchema.pre('save', async function(next){
     this.password = await bcrypt.hash(this.password, 10)
 })
 
-//return json
-userSchema.method.getJwtToken = function(){
-    jwt.sign({id: this._id}, process.env.JWT_SECRET,{
-        expiresIn: process.env.JWT_EXPIRES_TIME
-    })
-}
+// //return json
+// userSchema.method.getJwtToken = function(){
+//     jwt.sign({id: this._id}, process.env.JWT_SECRET,{
+//         expiresIn: process.env.JWT_EXPIRES_TIME
+//     })
+// }
 
 
 
